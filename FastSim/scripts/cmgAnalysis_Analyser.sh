7c758a968db94f84e6555b39cd09be81cfd42b9c
mgouzevi <>
2011-10-28 13:42:19 +0000

    Useful scripts

---
+++ FastSim/scripts/cmgAnalysis_Analyser.sh
@@ -0,0 +1,93 @@
+REP=$1
+PART=$2
+ENERGY=$3
+REGION=$4
+NFILES=$5
+NEVENTS=$6
+PRODUCE=$7
+QUEUE=$8
+
+if [ $NFILES -le 0 ] 
+then
+    NFILES=20
+fi
+
+if [ $NEVENTS -le 0 ] 
+then
+    NEVENTS=1000
+fi
+
+if [ $# -le 7 ]
+then
+  echo "Usage: `basename $0` {Repertory ProdName SourceNameForOutRep bProduce DataType}"
+  exit
+fi
+
+PTITLE=$PART
+
+case $PART in
+11) PTITLE='ELECTRON';;
+12) PTITLE="NEUTRINO";;
+22) PTITLE="PHOTON";;
+211) PTITLE='PION';;
+*) echo "This particle is not in the list of cmgAnalysis_Analyser.sh, you may want to update it. For the moment we name the repertory with "$PART". Do you want to continue? Y or N"
+	read answer
+	if [ $answer -eq 'Y' ] 
+	    then
+	    PTITLE=$PART
+	else 
+	    echo "stop the script"
+	    exit
+	fi;;
+esac
+
+OUT=${REP}'/'${PTITLE}'/'${ENERGY}'GEV/'${REGION}
+
+echo "------------------------------------"
+echo "------------------------------------"
+echo "CASTOR ROOT REPERTORY "$REP
+echo "FILES Store repertory "$OUT
+echo "Particle with PdgId ="$PART" is "$PTITLE
+echo "Energy ="$ENERGY
+echo "Calorimeter region = "$REGION
+echo "Number of files to produce "$NFILES
+echo "with "$NEVENTS" events"
+echo "have to be produced = "$PRODUCE
+echo "------------------------------------"
+echo "------------------------------------"
+
+MINE=$[ENERGY-1]
+
+
+sed s/'#PART '/''/ <SinglePigun_FASTSIM_cfg.py | 
+sed s/'#MIN'/${MINE}/ |
+sed s/'#MAX'/${ENERGY}/ | 
+sed s/'#'$REGION' '/''/ |
+sed s/'#ENERGY '/''/ |
+sed s/'#PARTID'/$PART/ |
+sed s/'#MEVENTS '/''/ |
+sed s/'#NEVENTS'/$NEVENTS/ |
+sed s/'#FILENAME '/''/ >SinglePigun_FASTSIM_launch_cfg.py
+
+
+
+if [ $PRODUCE -eq 1 ] 
+then
+    OUT=${REP}'/'${PTITLE}
+    nsmkdir $OUT
+    OUT=$OUT'/'${ENERGY}'GEV'
+    nsmkdir $OUT
+    OUT=$OUT'/'${REGION}
+    nsmkdir $OUT
+    echo "We are launching the production for "$OUT
+    
+    cmsBatch.py $NFILES SinglePigun_FASTSIM_launch_cfg.py  -o OutResonance_$PTITLE_$ENERGY_$REGION -r ${OUT} -b 'bsub -q '$QUEUE' < batchScript.sh'
+else 
+    echo "All files"
+#    nsls -l /castor/cern.ch/user/m/mgouzevi/cmst3/JetAndMetAnalyses/$REP/$PROD/$TITLE
+#    echo "Only PFAnalysis Files"
+#    nsls -l /castor/cern.ch/user/m/mgouzevi/cmst3/JetAndMetAnalyses/$REP/$PROD/$TITLE | grep "diJetAnalysis_"
+#    cd $CMSSW_BASE/src/CMGTools/DiJetHighMass/prod
+#    ~/bin/importNewSource.py /$REP/$PROD --castorBaseDir ${castorBaseDir} -p "diJetAnalysis_"$TITLE"_([0-9]|[1-9][0-9]).*root" -f $TITLE
+#    cd -
+fi
---
+++ FastSim/scripts/cmgAnalysis_launch.sh
@@ -0,0 +1,38 @@
+# The root of the castor repertory where you want to store your data. This
+# repertory must exist
+REP=/castor/cern.ch/cms/store/cmst3/user/mgouzevi/FCAL_UPGRADE
+# The PDG id of your particle. The script would automatically transform
+# it into particle name when known and add to the repertory name
+PART=11
+# Energy f your particle in GeV
+ENERGY=10
+# Region of the calorimeter where you want to fire your aprticle. 2 options possible: CENTRAL (-0.9 < eta < 0.9) or FORWARD (1.6 < eta < 2.4)
+REGION=FORWARD
+#Number of files you want to generate
+NFILES=10
+#Numer of events per file
+NEVENTS=2000
+# Produce those files (1) or create a source file (0)?
+PRODUCE=1
+# Batch queue - 
+QUEUE=8nh
+
+
+i=2
+while [ $i -le 8 ]; do
+
+case $i in
+1) ENERGY=10;;
+2) ENERGY=20;;
+3) ENERGY=40;;
+4) ENERGY=80;;
+5) ENERGY=160;;
+6) ENERGY=320;;
+7) ENERGY=640;;
+esac
+
+../scripts/cmgAnalysis_Analyser.sh $REP $PART $ENERGY $REGION $NFILES $NEVENTS $PRODUCE $QUEUE
+
+i=$[i+1]
+done
+
