dcd203468bd4f5d0a08666c7da8d4a56fd4a39d9
cowden <>
2012-02-12 16:42:52 +0000

    This commit was manufactured by cvs2svn to create tag 'V00_01_01'.
    Sprout from master 2012-02-12 16:42:51 UTC andreevv 'Add shower profile histograms'
    Cherrypick from master 2011-10-25 20:36:16 UTC mgouzevi 'Calo analyser updated to describe the average calorimeter width':
        FastSim/BuildFile.xml
        FastSim/interface/CaloAnalyzer.h
        FastSim/src/CaloAnalyzer.cc
        FastSim/test/SinglePigun_FASTSIM_cfg.py
        FastSim/test/caloanalyzer_cfg.py
    Delete:
        FastSim/python/LSO_cff.py
        FastSim/python/Samples/EF_LC_4TMS_ELECTRON_10_FORWARD.py
        FastSim/python/Samples/EF_LC_4TMS_ELECTRON_160_FORWARD.py
        FastSim/python/Samples/EF_LC_4TMS_ELECTRON_20_FORWARD.py
        FastSim/python/Samples/EF_LC_4TMS_ELECTRON_320_FORWARD.py
        FastSim/python/Samples/EF_LC_4TMS_ELECTRON_40_FORWARD.py
        FastSim/python/Samples/EF_LC_4TMS_ELECTRON_640_FORWARD.py
        FastSim/python/Samples/EF_LC_4TMS_ELECTRON_80_FORWARD.py
        FastSim/python/Samples/ELECTRON_10_FORWARD.py
        FastSim/python/Samples/ELECTRON_160_FORWARD.py
        FastSim/python/Samples/ELECTRON_20_FORWARD.py
        FastSim/python/Samples/ELECTRON_320_FORWARD.py
        FastSim/python/Samples/ELECTRON_40_FORWARD.py
        FastSim/python/Samples/ELECTRON_640_FORWARD.py
        FastSim/python/Samples/ELECTRON_80_FORWARD.py
        FastSim/python/Samples/STANDARD_PHOTON_10_FORWARD.py
        FastSim/python/Samples/STANDARD_PHOTON_160_FORWARD.py
        FastSim/python/Samples/STANDARD_PHOTON_20_FORWARD.py
        FastSim/python/Samples/STANDARD_PHOTON_320_FORWARD.py
        FastSim/python/Samples/STANDARD_PHOTON_40_FORWARD.py
        FastSim/python/Samples/STANDARD_PHOTON_640_FORWARD.py
        FastSim/python/Samples/STANDARD_PHOTON_80_FORWARD.py
        FastSim/scripts/Kill.sh
        FastSim/scripts/caloanalyzer_launch_cfg.py
        FastSim/scripts/massNsCheckFileValidity.sh
        FastSim/scripts/singleGun_Analyser.sh
        FastSim/scripts/singleGun_launch.sh
        FastSim/test/Macros/FitterExample.C
        FastSim/test/Macros/PlotterShower.C
        GEANT/GNUmakefile
        GEANT/README
        GEANT/currentEvent.rndm
        GEANT/currentRun.rndm
        GEANT/hadr01.in
        GEANT/hadupg.cc
        GEANT/include/DetectorConstruction.hh
        GEANT/include/EventAction.hh
        GEANT/include/HistoManager.hh
        GEANT/include/PrimaryGeneratorAction.hh
        GEANT/include/RunAction.hh
        GEANT/include/SteppingAction.hh
        GEANT/src/DetectorConstruction.cc
        GEANT/src/EventAction.cc
        GEANT/src/HistoManager.cc
        GEANT/src/PrimaryGeneratorAction.cc
        GEANT/src/RunAction.cc
        GEANT/src/SteppingAction.cc
        GEANT/test_01.root
        GEANT/test_out01
        GEANT/vis.mac

--- FastSim/test/caloanalyzer_cfg.py
+++ FastSim/test/caloanalyzer_cfg.py
@@ -6,15 +6,14 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load('FastSimulation.Configuration.Geometries_MC_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.GlobalTag.globaltag = 'MC_42_V15A::All'
-#process.GlobalTag.globaltag = 'MC_50_V13::All' 
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-    'file:SinglePigun_FASTSIM.root'
+#    'file:/tmp/mgouzevi/SinglePigun_FASTSIM.root'
+    'rfio:/castor/cern.ch/cms/store/cmst3/user/mgouzevi/FCAL_UPGRADE/PIGUN/10GEV_ENDCAP/SinglePigun_FASTSIM_0.root'
     )
 )
 
@@ -23,8 +22,3 @@ process.demo = cms.EDAnalyzer('CaloAnalyzer'
 
 
 process.p = cms.Path(process.demo)
-
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
-
-#SOURCE process.load('#SRC')
-
