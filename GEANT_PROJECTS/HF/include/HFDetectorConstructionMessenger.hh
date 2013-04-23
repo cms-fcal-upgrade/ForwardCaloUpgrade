f13dfaf112c6929a090cb3e67e9678e85adbe037
heli <>
2013-04-23 17:43:59 +0000

    replacement of class HFMessenger.

---
+++ GEANT_PROJECTS/HF/include/HFDetectorConstructionMessenger.hh
@@ -0,0 +1,35 @@
+#ifndef HF_HFDetectorConstructionMessenger_hh
+#define HF_HFDetectorConstructionMessenger_hh
+
+#include "G4UImessenger.hh"
+#include "globals.hh"
+
+class HFDetectorConstruction;
+class G4UIdirectory;
+class G4UIcmdWithAnInteger;
+class G4UIcmdWithADoubleAndUnit;
+class G4UIcmdWithADouble;
+class G4UIcmdWithAString;
+
+class HFDetectorConstructionMessenger: public G4UImessenger
+{
+  public:
+   HFDetectorConstructionMessenger(HFDetectorConstruction*);
+   ~HFDetectorConstructionMessenger();
+
+   void SetNewValue(G4UIcommand*, G4String);
+
+  private:
+   HFDetectorConstruction * m_detector;
+   G4UIdirectory * m_dir;
+
+   // the commands
+   G4UIcmdWithAnInteger * m_nRodCmd;
+   G4UIcmdWithADoubleAndUnit * m_rRodCmd;
+   G4UIcmdWithADoubleAndUnit * m_lengthCmd;
+   G4UIcmdWithADouble * m_fibreIndexCmd;  // fibre index of refraction
+   G4UIcmdWithADouble * m_cladIndexCmd;
+
+};
+
+#endif
